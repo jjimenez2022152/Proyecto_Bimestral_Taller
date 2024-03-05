b044e41bdef46bd5933452859c4ac1822745e35f
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-03-04 22:06:49 -0600

    Se crea el modelo Product

---
+++ src/product/product.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Categorie',
+        required: true
+    }
+});
+
+export default mongoose.model('Product', productSchema );
