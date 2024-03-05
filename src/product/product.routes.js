1d34858d7d32925c04b318cdad5f083be8177016
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-03-04 22:31:47 -0600

    Se agrega el archivo product.routes.js aun no funcional

---
+++ src/product/product.routes.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { validarJWT } from "../middlewares/validar-jwt.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
+//import { tieneRole } from "../middlewares/validar-roles.js";
+import { productPost, productPut } from "./product.controller.js"; 
+const router = Router();
+
+router.post(
+  "/", 
+  [
+    validarJWT, 
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("description", "La descripcion es obligatoria").not().isEmpty(),
+    check("price", "El precio es de caracter obligatorio").not().isEmpty(),
+    check("stock", "El stock es obligatorio").not().isEmpty(),
+    check("category", "La caregoria es obligatoria").not().isEmpty(),
+    validarCampos, 
+  ],
+  productPost 
+);
+
+router.put(
+  "/:id", 
+  [
+    validarJWT, 
+    check("id", "No es un ID válido").isMongoId(),
+    //check("id").custom(existeUsuarioById),
+    validarCampos, 
+  ],
+  productPut 
+);
+
+
+export default router;
